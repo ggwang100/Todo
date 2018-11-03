0f10683d07bc6ff7c52d205fda6bc02481989174
Ko JoungYong <gjy1357@gmail.com>
2018-11-03 19:15:15 +0900

    read 자바 코드 수정 및 인텐트 생성

--- app/src/main/java/com/example/gwangtae/todo/read.java
+++ app/src/main/java/com/example/gwangtae/todo/read.java
@@ -1,4 +1,31 @@
 package com.example.gwangtae.todo;
 
-public class read {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class read extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.read);
+
+    }
+
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.btn_update) { // 추가 버튼
+
+            Intent intent = new Intent(this, edit_record.class);
+            startActivity(intent);
+        }
+
+        if (id == R.id.btn_delete) { // 추가 버튼
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
 }
