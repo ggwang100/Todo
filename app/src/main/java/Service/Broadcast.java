6b36d6f3cd66309a501763480b19e4a7f5329c2d
Eungho <keh_as@naver.com>
2018-11-04 02:01:09 +0900

    브로드캐스트리시버로 재부팅시 앱 실행하도록 설정

--- app/src/main/java/Service/Broadcast.java
+++ app/src/main/java/Service/Broadcast.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.gwangtae.todo.MainActivity;
 
@@ -11,10 +12,11 @@ public class Broadcast extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {   // 재부팅 완료되면
-            Log.d("succ", "재부팅 완료");
-            Intent i = new Intent(context, MainActivity.class); // 시작
-            context.startService(i);
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {   // 재부팅 완료되면
+            Toast.makeText(context, "재부팅 완료", Toast.LENGTH_SHORT).show();
+            Intent i= new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
         }
     }
 }
